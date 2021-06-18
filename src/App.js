9b683c275c0edb6eee789f6ce83be681223e5c20
Rosa Pavón <mrosapavon@gmail.com>
2021-06-18 16:06:26 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [compositores, setCompositores] = useState([
+  'Wolfgang Amadeus Mozart',
+  'Ludwig van Beethoven',
+  'Johann Sebastian Bach']);
+
+ const [datos, setDatos] = useState({//aqui es donde vamos a almacenar lo que indique el usuario en el imput
+   nombre:''
+ })
+
+/* const handleInputChange = (event) => {
+   console.log(event.target.value)
+   setDatos({
+     ...datos,//generamos una copia para no perder ningun dato
+     [event.target.name] : event.target.value
+
+   })
+ } */ 
+
+ const handleInputChange = (event) => {
+  const autor= event.target.value
+  setCompositores([...compositores, autor])
+}
+const handleInputDelete = (name) => {
+  const newCompositor = compositores.filter((autor) => autor.name !== name)
+}
+
+  
+/* const añadirCompositor= compositores.map((musico, index)=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+     <li>{musico}</li>
+  </ul>
   );
+
+}) */
+
+    return (
+    <>
+    <h1>Formularios en react</h1>
+    <form>
+      <div>
+            <input
+             type="text" 
+             name="nombre"
+             onChange={handleInputChange} />
+      <button type="submit">Pulsa</button>
+      </div>
+      <div>
+            <input
+             type="text" 
+             name="name"
+             onChange={handleInputDelete} />
+      <button type="submit">Elimina</button>
+      </div>
+      </form>
+      <h3>{datos.nombre}</h3>
+      {/* <h3>{añadirCompositor}</h3> */}
+      
+    </>
+  )
 }
 
 export default App;
